af2998cbce573553db10f020e022b3a2a8410590
josue <josue2017ivan@gmail.com>
2026-07-09 13:32:23 -0600

    agregue parte del menu

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
         EmpresaMensajeria empresaMensajeria = new EmpresaMensajeria();
@@ -11,6 +12,17 @@ public class Main {
         empresaMensajeria.RegistrarPaquete(new PaqueteEnvio("123",32,new EnvioEstandar()));
 
         empresaMensajeria.reporte();
+        int  opcion;
+        do{
+            System.out.println("1. ");
+            System.out.println("2. ");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            opcion = sc.nextInt();
+        }while(opcion!=4);
+
+        sc.close();
+
 
     }
 }
